0c1c8736bcfc7895800364fd191d543008ffbb35
pSbral <80040598+pSbral@users.noreply.github.com>
2022-09-20 09:47:33 -0300

    Add files via upload

---
+++ Aula1309/js/array.js
@@ -0,0 +1,15 @@
+const kkkkkj = ['eu', 'tu', 'nois', 'bota', 'nela']
+console.log(kkkkkj)
+
+kkkkkj.push('o bonde chegou os predado de perereca')
+console.log(kkkkkj)
+
+kkkkkj.pop()
+console.log(kkkkkj)
+
+kkkkkj.unshift('nois')
+console.log(kkkkkj)
+
+kkkkkj.splice(2,2)
+console.log(kkkkkj)
+
--- Aula1309/js/calcularXP.js
+++ Aula1309/js/calcularXP.js
@@ -1,7 +1,18 @@
 const btnCalcular = document.querySelector('#btnCalcular')
 
-function mensagem(){
-    alert('vai se fuder sua vagabunda')
-}
+btnCalcular.addEventListener('click', () => {
+            const herois = document.querySelectorAll('.heroi')
+            herois.forEach(heroi => {
+                let forca = Number(heroi.querySelector('.forca').textContent)
+                let agilidade = Number(heroi.querySelector('.agilidade').textContent)
+                let mana = Number(heroi.querySelector('.mana').textContent)
 
-btnCalcular.addEventListener('click', mensagem)
+                let xpFinal = (forca + mana + agilidade) / 3
+
+                heroi.querySelector('.xp').textContent = xpFinal.toFixed(0)
+
+                if (xpFinal < 45) {
+                    heroi.classList.add('bg-danger', 'text-light')
+                }
+            });
+    })
